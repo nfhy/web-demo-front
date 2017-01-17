eeac0232b3cabc0e8645499575f7bee5c8f16a8a
nfhy <836425475@qq.com>
2017-01-17 18:50:23 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by wangll on 2017/1/17.
+ */
+'use strict';
+
+$(document).ready(function() {
+    $('#dic_form_sub')
+        .bind('click',
+            function() {
+                var req_data = {'dicCode' : 'DT', 'params' : ''};
+                req_data.t = new Date().getTime();
+                $.ajax({
+                    url: 'http://localhost:8080/web-demo/expose/input.do',
+                    method: 'POST',
+                    dataType: 'JSON',
+                    contentType:'application/json;charset=UTF-8',//关键是要加上这行
+                    data: JSON.stringify(req_data),
+                    success: function () {
+                    }
+                });
+            }
+        );
+});
