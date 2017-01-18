599654803ddf100717fda0eb64f756c263a852fe
nfhy <836425475@qq.com>
2017-01-18 17:37:58 +0800

    123

--- js/main.js
+++ js/main.js
@@ -10,12 +10,13 @@ $(document).ready(function() {
                 var req_data = {'dicCode' : 'DT', 'params' : ''};
                 req_data.t = new Date().getTime();
                 $.ajax({
-                    url: 'http://localhost:8080/web-demo/expose/input.do',
+                    url: 'http://localhost:8080/expose/cache/DT.do',
                     method: 'POST',
                     dataType: 'JSON',
                     contentType:'application/json;charset=UTF-8',//关键是要加上这行
                     data: JSON.stringify(req_data),
                     success: function () {
+                        $.session.get('testSA');
                     }
                 });
             }
